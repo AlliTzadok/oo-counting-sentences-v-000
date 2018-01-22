8cafad85f5b7b008f6a17e47358c663267fb0398
Alli Tzadok <allisontzadok@gmail.com>
2018-01-22 20:03:26 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/\!|\?|\./).delete_if {self.size < 2}
-    self.size
-
+    self.split(/\!|\?|\./).delete_if { self.size < 2}.size
   end
 end
